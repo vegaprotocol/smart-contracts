0c35fa9c8deb2a024ba1365d4204463d5261ff2c
Edd <edd@vega.xyz>
2021-09-14 18:16:18 +0100

    Update with latest contracts

--- contracts/ETH_Asset_Pool.sol
+++ contracts/ETH_Asset_Pool.sol
@@ -2,12 +2,11 @@
 pragma solidity 0.8.1;
 
 import "./IMultisigControl.sol";
-import "./IERC20.sol";
 
-/// @title ERC20 Asset Pool
+/// @title ETH Asset Pool
 /// @author Vega Protocol
-/// @notice This contract is the target for all deposits to the ERC20 Bridge via ERC20_Bridge_Logic
-contract ERC20_Asset_Pool {
+/// @notice This contract is the target for all deposits to the ETH Bridge via ETH_Bridge_Logic
+contract ETH_Asset_Pool {
 
     event Multisig_Control_Set(address indexed new_address);
     event Bridge_Address_Set(address indexed new_address);
@@ -15,8 +14,8 @@ contract ERC20_Asset_Pool {
     /// @return Current MultisigControl contract address
     address public multisig_control_address;
 
-    /// @return Current ERC20_Bridge_Logic contract address
-    address public erc20_bridge_address;
+    /// @return Current ETH_Bridge_Logic contract address
+    address public ETH_bridge_address;
 
     /// @param multisig_control The initial MultisigControl contract address
     /// @notice Emits Multisig_Control_Set event
@@ -37,7 +36,7 @@ contract ERC20_Asset_Pool {
         emit Multisig_Control_Set(new_address);
     }
 
-    /// @param new_address The new ERC20_Bridge_Logic contract address.
+    /// @param new_address The new ETH_Bridge_Logic contract address.
     /// @param nonce Vega-assigned single-use number that provides replay attack protection
     /// @param signatures Vega-supplied signature bundle of a validator-signed set_bridge_address order
     /// @notice See MultisigControl for more about signatures
@@ -45,19 +44,18 @@ contract ERC20_Asset_Pool {
     function set_bridge_address(address new_address, uint256 nonce, bytes memory signatures) public {
         bytes memory message = abi.encode(new_address, nonce, 'set_bridge_address');
         require(IMultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
-        erc20_bridge_address = new_address;
+        ETH_bridge_address = new_address;
         emit Bridge_Address_Set(new_address);
     }
 
-    /// @notice This function can only be run by the current "multisig_control_address" and, if available, will send the target tokens to the target
-    /// @param token_address Contract address of the ERC20 token to be withdrawn
-    /// @param target Target Ethereum address that the ERC20 tokens will be sent to
-    /// @param amount Amount of ERC20 tokens to withdraw
-    /// @dev amount is in whatever the lowest decimal value the ERC20 token has. For instance, an 18 decimal ERC20 token, 1 "amount" == 0.000000000000000001
+    /// @notice This function can only be run by the current "multisig_control_address" and, if available, will send the target eth to the target
+    /// @param target Target Ethereum address that the ETH will be sent to
+    /// @param amount Amount of ETH to withdraw
+    /// @dev amount is in wei, 1 wei == 0.000000000000000001 ETH
     /// @return true if transfer was successful.
-    function withdraw(address token_address, address target, uint256 amount) public returns(bool) {
-        require(msg.sender == erc20_bridge_address, "msg.sender not authorized bridge");
-        require(IERC20(token_address).transfer(target, amount), "token transfer failed");
+    function withdraw(address payable target, uint256 amount) public returns(bool) {
+        require(msg.sender == ETH_bridge_address, "msg.sender not authorized bridge");
+        target.transfer(amount);
         return true;
     }
 }
